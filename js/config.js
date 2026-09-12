51411551b5de976267951f94a6dafae5b8ca857a
alfiananandaid <alfianananda.id@gmail.com>
2026-09-12 13:48:54 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
  * GANTI API_URL dengan URL deployment Web App GAS Anda (diakhiri /exec).
  */
 window.SOM_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxyk02-_D7hS15PfVGhNE60NkziQiSnk2bywzzLLRY1TDjV6zw8yuQLmJbzHlpxcctAVA/exec',
   SESSION_DURATION_MS: 12 * 60 * 60 * 1000,
   APP_VERSION: '1.0.0'
 };
